e0789fe55c5a6224378d5c597d761ffc986e3992
Keenan Ng <keenanng.dev@gmail.com>
2021-02-02 22:35:19 -0800

    factorial

--- solutions/day-01/level1.js
+++ solutions/day-01/level1.js
@@ -1,9 +1,7 @@
 // ##### Exercises: Level 3
-/*
-3. Call your function _shuffleArray_, it takes an array as a parameter and
-it returns a shuffled array
-*/
 
+// 3. Call your function _shuffleArray_, it takes an array as a parameter and
+// it returns a shuffled array
 const shuffleArray = (array) => {
   for (let i = 0; i < array.length; i++) {
     let randomNum = Math.floor(Math.random() * array.length);
@@ -12,4 +10,15 @@ const shuffleArray = (array) => {
     array[randomNum] = tempStorage;
   }
   return array;
+}
+
+// 4. Call your function _factorial_, it takes a whole number as a parameter and
+// it return a factorial of the number
+const factorial = (n) => {
+  let result = 1
+  while (n > 1) {
+    result *= (n - 1)
+    n--
+  }
+  return result;
 }
