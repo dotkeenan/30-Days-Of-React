7091c01c91e76e7a7c11a16c397e78c94317202b
Keenan Ng <keenanng.dev@gmail.com>
2021-02-08 22:07:18 -0800

    inline styles for day 3 react

--- solutions/day-03/day3/src/index.js
+++ solutions/day-03/day3/src/index.js
@@ -1,28 +1,66 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const jsxElement = <h1>This is a JSX element</h1>
+// const jsxElement = <h1>This is a JSX element</h1>
+// const title = <h2>Getting Started React</h2>
+
+const headerStyles = {
+  backgroundColor: '#61DBFB',
+  fontFamily: 'Helvetica Neue',
+  padding: 25,
+  lineHeight: 1.5,
+}
+
+const header = (
+  <header style={headerStyles}>
+    <div className='header-wrapper'>
+      <h1>Welcome to 30 Days of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>dotkeenan</p>
+      <small>Feb 8, 2020</small>
+    </div>
+  </header>
+)
+
+const mainStyles = {
+  backgroundColor: '#F3F0F5',
+}
+
+const main = (
+  <main style={mainStyles}>
+    <p>Prerequisite to get started react.js:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+)
+
+const footerStyles = {
+  backgroundColor: '#61DBFB',
+}
+
+// JSX element, footer
+const footer = (
+  <footer style={footerStyles}>
+    <p>Copyright 2020</p>
+  </footer>
+)
+
+// JSX element, app, a container or a parent
+const app = (
+  <div className='app'>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
 const rootElement = document.getElementById('root')
-
-ReactDOM.render(jsxElement, rootElement)
-
-
-
-
-/* import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(); */
+// we render the JSX element using the ReactDOM package
+// ReactDOM has the render method and the render method takes two argument
+ReactDOM.render(app, rootElement)
+// or
+//  ReactDOM.render([header, main, footer], rootElement)
